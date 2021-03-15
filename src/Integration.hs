9dea5f45c674381c3301ff231a3787c6c24d5ad8
Yehor Polishchuk <e-dp@ukr.net>
2021-03-15 19:16:06 +0200

    integration in monad added (with logging)

--- src/Integration.hs
+++ src/Integration.hs
@@ -11,19 +11,34 @@ integrate f a b n = let
         | otherwise = sum (acc + f curXi) lastInd (curInd+1) (curXi+dx)
     in dx * ((sum 0.0 (n-1) 1 (a+dx) ) + ((f b + f a) / 2))
 
+
 integrateWithEps f a b n eps = let
     integrateWithEpsInner f a b n eps prevEps = let
         res1 = integrate f a b n
         res2 = integrate f a b (n*2)
-        curEps = abs $ res1 - res2 
+        curEps = abs $ res1 - res2
         in if (abs curEps <= eps) then res2 else 
             if (curEps > prevEps) then res1 else 
                 integrateWithEpsInner f a b (n*2) eps curEps
     in integrateWithEpsInner f a b n eps 1000
 
+integrateWithEpsM f a b n eps prevEps = do
+    let res1 = integrate f a b n
+    let res2 = integrate f a b (n*2)
+    let curEps = abs $ res2 - res1
+    let evalEps = 0.3 * curEps
+    putStrLn $ "res1: " ++ show res1 ++ "\n" ++
+        "res2: " ++ show res2 ++ "\n" ++
+        "curEps: " ++ show curEps ++ "\n\n"
+    if (abs evalEps <= eps) then do
+        putStrLn $ show res2 else 
+            if (abs evalEps > prevEps) then do putStrLn $ show res1 else do
+                integrateWithEpsM f a b (n*2) eps evalEps
 
 integrateTest1 = integrate (\x -> x) 0 5 5
 
 integrateTest2 = integrateWithEps (\x-> x) 0 5 5 0.000001
 
 integrateTest3 = integrateWithEps sin 0 pi 100000 0.000001
+
+integrateTest3M = integrateWithEpsM sin 0 pi 20 0.000001 100
