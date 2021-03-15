da4251a9d29688f6f82eeb329ebbaef1395101fc
Yehor Polishchuk <e-dp@ukr.net>
2021-03-15 16:06:27 +0200

    integral calculation function with trapezoid rule implemented

--- src/CommandLineInterface.hs
+++ src/CommandLineInterface.hs
@@ -1,7 +1,11 @@
 module CommandLineInterface (cli) where
 
+import Text.Read
+
 import Integration
+-- import System.Eval.Haskell
 
 cli :: IO ()
 cli = do 
-    putStrLn "Hello"
+    putStrLn "Choose the function to integrate:\n1.sin(x)\n2.f(x)=x^2"
+
--- src/Integration.hs
+++ src/Integration.hs
@@ -1,4 +1,29 @@
 module Integration where
+-- module Integration (integrateWithEps) where
 
 import Control.Parallel
 
+-- https://en.wikipedia.org/wiki/Trapezoidal_rule
+integrate f a b n = let 
+    dx = (b-a) / n
+    sum acc lastInd curInd curXi
+        | (curInd > lastInd) || (curXi >b-dx) = acc
+        | otherwise = sum (acc + f curXi) lastInd (curInd+1) (curXi+dx)
+    in dx * ((sum 0.0 (n-1) 1 (a+dx) ) + ((f b + f a) / 2))
+
+integrateWithEps f a b n eps = let
+    integrateWithEpsInner f a b n eps prevEps = let
+        res1 = integrate f a b n
+        res2 = integrate f a b (n*2)
+        curEps = abs $ res1 - res2 
+        in if (abs curEps <= eps) then res2 else 
+            if (curEps > prevEps) then res1 else 
+                integrateWithEpsInner f a b (n*2) eps curEps
+    in integrateWithEpsInner f a b n eps 1000
+
+
+integrateTest1 = integrate (\x -> x) 0 5 5
+
+integrateTest2 = integrateWithEps (\x-> x) 0 5 5 0.000001
+
+integrateTest3 = integrateWithEps sin 0 pi 100000 0.000001
