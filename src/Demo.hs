e46836edf8b4432225c151a5350a9a8195f47156
Yehor Polishchuk <e-dp@ukr.net>
2021-03-16 12:22:18 +0200

    parallel algorithm for error approximation added

---
+++ src/Demo.hs
@@ -0,0 +1,19 @@
+module Demo where 
+
+import Integration
+
+
+-- one-thread (x)
+integrateTest2 = integrateWithEps (\x-> x) 0 5 5 0.000001
+
+-- one-thread calc (sin)
+integrateTest3 = integrateWithEps sin 0 pi 100000 0.000001
+
+-- parallel calculations (sin)
+integrateTest3P = integrateWithEpsPar sin 0 pi 100000 0.000001
+
+-- logging calculations (sin)
+integrateTest3M = integrateWithEpsM sin 0 pi 20 0.000001 100
+
+-- one-thread x^2
+integrateTest4 = integrateWithEps (\x -> x^2) 1 5 100000 0.000001
--- src/Integration.hs
+++ src/Integration.hs
@@ -1,9 +1,12 @@
 module Integration where
--- module Integration (integrateWithEps) where
 
 import Control.Parallel
 
--- https://en.wikipedia.org/wiki/Trapezoidal_rule
+{-|
+ Integration with trapezoidal rule (https://en.wikipedia.org/wiki/Trapezoidal_rule).
+ Used uniform grid with n eqully spaced segments.
+ One-threaded.
+-}
 integrate f a b n = let 
     dx = (b-a) / n
     sum acc lastInd curInd curXi
@@ -12,6 +15,11 @@ integrate f a b n = let
     in dx * ((sum 0.0 (n-1) 1 (a+dx) ) + ((f b + f a) / 2))
 
 
+{-|
+    Sequential integration with Trapezoidal rule.
+    Error is evaluated with Runge's rule.
+    One-threaded.
+-}
 integrateWithEps f a b n eps = let
     integrateWithEpsInner f a b n eps prevEps = let
         res1 = integrate f a b n
@@ -22,23 +30,36 @@ integrateWithEps f a b n eps = let
                 integrateWithEpsInner f a b (n*2) eps curEps
     in integrateWithEpsInner f a b n eps 1000
 
+
+{-|
+    See 'integrateWithEps' function docs.
+    Logging added.
+    Parameter 'prevEps' should be set to a big number 
+        [bigger than 'eps' (like 100)].
+-}
 integrateWithEpsM f a b n eps prevEps = do
     let res1 = integrate f a b n
     let res2 = integrate f a b (n*2)
     let curEps = abs $ res2 - res1
     let evalEps = 0.3 * curEps
-    putStrLn $ "res1: " ++ show res1 ++ "\n" ++
-        "res2: " ++ show res2 ++ "\n" ++
+    putStrLn $ "I1: " ++ show res1 ++ "\n" ++
+        "I2: " ++ show res2 ++ "\n" ++
         "curEps: " ++ show curEps ++ "\n\n"
     if (abs evalEps <= eps) then do
         putStrLn $ show res2 else 
             if (abs evalEps > prevEps) then do putStrLn $ show res1 else do
                 integrateWithEpsM f a b (n*2) eps evalEps
 
-integrateTest1 = integrate (\x -> x) 0 5 5
 
-integrateTest2 = integrateWithEps (\x-> x) 0 5 5 0.000001
-
-integrateTest3 = integrateWithEps sin 0 pi 100000 0.000001
-
-integrateTest3M = integrateWithEpsM sin 0 pi 20 0.000001 100
+{-|
+    Parallel version of 'integrateWithEps' function.
+-}
+integrateWithEpsPar f a b n eps = let
+    integrateWithEpsInner f a b n eps prevEps = let
+        res1 = integrate f a b n
+        res2 = integrate f a b (n*2)
+        curEps = par res1 (pseq res2 (abs $ res1 - res2))
+        in if (abs curEps <= eps) then res2 else 
+            if (curEps > prevEps) then res1 else 
+                integrateWithEpsInner f a b (n*2) eps curEps
+    in integrateWithEpsInner f a b n eps 1000
